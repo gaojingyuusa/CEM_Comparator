0648b564c3b795647e7416e12d26fef3dae4a260
高靖宇 <kouseiu@Jingyu.home>
2019-07-17 15:05:24 -0400

    Continue adding further features

--- ui.R
+++ ui.R
@@ -31,14 +31,38 @@ fluidPage(
     sidebarPanel(id="sidebar",
                  h3(strong("Customization"),style="color:#002244"),
                  sliderInput("YEAR","Select Period", min=1960, max=2018,value=c(2012,2017)),
-                 selectInput("TARGET","Select Target Country",choices=unique(data_file$countryname),"Afghanistan", multiple = F)
-                
+                 selectInput("TARGET","Select Target Country",choices=unique(data_file$countryname),"Afghanistan", multiple = F),
+                 textOutput("country.region"),
+                 textOutput("country.income"),
+                 textOutput("country.land"),
+                 textOutput("country.small"),
+                 textOutput("country.fcs"),
+                 checkboxInput("REGION", "Regional Peers", value=FALSE),
+                 checkboxInput("LAND", "Landlocked", value=FALSE),
+                 checkboxInput("SMALL", "Small States", value=FALSE),
+                 checkboxInput("FCS", "Fragile & Conflicted States", value=FALSE)
                 ),
     
     # Main Panel: Structural and Aspirational
     mainPanel(
       tabsetPanel(type="tabs",
-                  tabPanel("Structural Comparators"),
+                  tabPanel("Structural Comparators",
+                      # Set layout
+                      fluidRow(
+                         h2("Welcome to structural peers"),
+                         # Structural indicators
+                         column(3, style = "background-color:#4d3a7d;",
+                               "sidebar"
+                               ),
+                         # List of structural countries
+                         column(9, style = "background-color:red;",
+                                "main"
+                               )
+                        
+                              )     
+                           
+                           
+                          ),
                   tabPanel("Aspirational Comparators")
                  )
              )
