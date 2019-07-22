fad3dc4748d38c0c1b51d44b92f04135766f4c37
Jingyu Gao <jgao2@ifc.org>
2019-07-22 12:32:51 -0400

    Corrected naming errors on windows desktop

--- CEM_selector_data.R
+++ CEM_selector_data.R
@@ -18,13 +18,13 @@ text.fun <- function(data){
 
 # WDI file: data_file contains all the indicator data needed for the structural and aspirational comparators
 data_file <- read.csv("cem_full.csv", header=T) %>% text.fun()
-
+names(data_file)[1] <- "iso3"
 # WBG classification file: class_file contains information such as which country belongs to which group defined by WBG like region and income level
 class_file <- read.csv("iso_class.csv", header=T) %>% text.fun()
 
 # Indicator class file: indicator_file contains the description of each indicator used in the tool
 indicator_file <- read.csv("indicator.csv", header=T) %>% text.fun()
-
+names(indicator_file)[1] <- "Description"
 # Structural Break data from Miodrag
 struc_break_file <- read.csv("CEM_stru_break.csv", header=T) %>% text.fun()
 
